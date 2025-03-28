e21d73594d8914fad830ec6b3978cd04e6e2f51d
Tobos33 <pawkom.ps@gmail.com>
2025-03-28 19:05:46 +0100

    test2

--- GUI.java
+++ GUI.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     public GUI() throws HeadlessException{
-        this.setSize(1600,480);
+        this.setSize(1600,900);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
     public static void main(String[] args) {
