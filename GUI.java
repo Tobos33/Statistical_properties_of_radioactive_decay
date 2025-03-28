7c60b8ba7a317bc743f960fc87deb2b7b75da8f1
Paweł Strepikowski <157540205+Tobos33@users.noreply.github.com>
2025-03-28 15:57:46 +0100

    Add files via upload

---
+++ GUI.java
@@ -0,0 +1,19 @@
+package Main;
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI extends JFrame {
+    public GUI() throws HeadlessException{
+        this.setSize(640,480);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        GUI Frame = new GUI();
+        Frame.setVisible(true);
+    }
+}
